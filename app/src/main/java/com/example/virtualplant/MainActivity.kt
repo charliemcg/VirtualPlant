8a78eb6a367ed47d9d3d8b7dcdf975cdba886d8b
charliemcg <charlie_mcgregor@outlook.com>
2019-01-25 22:42:32 +0800

    Fixed bug where it was possible to reduce plant health simply by pausing and resuming without letting a day pass. Fixed it by updating the day timestamp in on resume instead on button click.

--- app/src/main/java/com/example/virtualplant/MainActivity.kt
+++ app/src/main/java/com/example/virtualplant/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.virtualplant
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Messenger
+import android.os.PersistableBundle
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.*
+import java.util.concurrent.TimeUnit
+import android.content.ComponentName
 
+
+
+//Created on 22/01/2019
 class MainActivity : AppCompatActivity() {
 
     var plantDead: Boolean = false
     var plantWatered: Boolean = false
+    var plantWateredToday: Boolean = false
     var plantHealth: Int = 0
     var plantSize: Int = 0
     var timePlanted: Long = 0
     val dayInMillis: Long = 86400000
+    val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,35 +40,30 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(tb)
     }
 
+    //method is called once per day
     fun anotherDay() {
+        //only update plant size and health if it is still alive
         if (!plantDead) {
-//            if (daysNotWatered == 0) {
+            //plant grows only if watered on previous day and not already fully grown
             if (plantSize < 4 && plantWatered) {
                 plantSize++
             }
-            tvPlantSize.setText("Plant size $plantSize")
-//            }
-//            if (plantWatered && daysNotWatered != 0) {
-//                daysNotWatered--
+//            tvPlantSize.setText("Plant size $plantSize")
+            //if plant not watered on previous day it's health is reduced. 0 == healthy, 4 == dead
             if (!plantWatered) {
                 plantHealth++
-                tvDaysNotWatered.setText("Days not watered $plantHealth")
+//                tvDaysNotWatered.setText("Days not watered $plantHealth")
+                //plant has died
                 if (plantHealth == 4) {
                     plantDead = true
-                    tvPlantDead.setText("Plant dead $plantDead")
+//                    tvPlantDead.setText("Plant dead $plantDead")
                 }
             }
         }
+        //water button returns every day
         btnWater.visibility = View.VISIBLE
         plantWatered = false
-//        if (getDiff() > dayInMillis) {
-//            Log.d("MainActivity", "diff: " + getDiff())
-//                timePlanted += dayInMillis
-//                daysNotWatered += (getDiff() / dayInMillis).toInt()
-//                daysNotWatered--
-//                tvDaysNotWatered.setText("Days not watered $daysNotWatered")
-//        }
-//        plantWatered = true
+        //setting the correct plant image based on size and health
         when (plantSize) {
             1 -> when (plantHealth) {
                 0 -> imgPlant.setImageDrawable(resources.getDrawable(R.drawable.plant_one_zero))
@@ -87,22 +96,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //water button clicked
     fun waterPlant(view: View) {
+        //remove button. can only water plant once per day
         btnWater.visibility = View.INVISIBLE
+        //watering the plant does nothing if plant has died
         if(!plantDead) {
+            //mark plant as watered so size and health can be updated on following day
             plantWatered = true
+            //increase plant health if not already full health
             if (plantHealth != 0) {
                 plantHealth--
-                tvDaysNotWatered.setText("Days not watered $plantHealth")
+//                tvDaysNotWatered.setText("Days not watered $plantHealth")
             }
+            //first time watering plant. set timestamp and inform user to water plant every day
             if (timePlanted == 0.toLong()) {
                 timePlanted = Calendar.getInstance().timeInMillis
+                Toast.makeText(this, "Water your plant every day", Toast.LENGTH_SHORT).show()
             } else {
-                timePlanted += dayInMillis
-                while (getDiff() > dayInMillis) {
-                    timePlanted += dayInMillis
-                }
+//                timePlanted += dayInMillis
+//                while (getDiff() > dayInMillis) {
+//                    timePlanted += dayInMillis
+//                }
             }
+        //inform user that plant is dead
+        }else{
+            Toast.makeText(this, "Your plant is dead \uD83D\uDE2D", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -111,6 +130,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    //reset the plant
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.miReset -> {
             plantDead = false
@@ -118,9 +138,9 @@ class MainActivity : AppCompatActivity() {
             plantHealth = 0
             plantSize = 0
             timePlanted = 0
-            tvPlantSize.setText("Plant size $plantSize")
-            tvDaysNotWatered.setText("Days not watered $plantHealth")
-            tvPlantDead.setText("Plant dead $plantDead")
+//            tvPlantSize.setText("Plant size $plantSize")
+//            tvDaysNotWatered.setText("Days not watered $plantHealth")
+//            tvPlantDead.setText("Plant dead $plantDead")
             btnWater.visibility = View.VISIBLE
             imgPlant.setImageDrawable(resources.getDrawable(R.drawable.plant_zero))
             true
@@ -128,6 +148,7 @@ class MainActivity : AppCompatActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
+    //determining if a day has passed
     fun getDiff(): Long {
         val now: Long = Calendar.getInstance().timeInMillis
         return now - timePlanted
@@ -136,11 +157,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         if (timePlanted == 0.toLong()) {
-            tvPlantSize.setText("Plant size $plantSize")
-            tvDaysNotWatered.setText("Days not watered $plantHealth")
-            tvPlantDead.setText("Plant dead $plantDead")
+//            tvPlantSize.setText("Plant size $plantSize")
+//            tvDaysNotWatered.setText("Days not watered $plantHealth")
+//            tvPlantDead.setText("Plant dead $plantDead")
         } else if (getDiff() > dayInMillis) {
             anotherDay()
+            timePlanted += dayInMillis
         }
     }
 
